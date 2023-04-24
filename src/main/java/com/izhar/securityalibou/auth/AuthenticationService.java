de6d41bc3e62054ce8e33a34a794a6f8ccc172f8
izharj <eezhar@gmail.com>
2023-04-24 17:32:14 +0800

    created authservice class and implemented  the register method

---
+++ src/main/java/com/izhar/securityalibou/auth/AuthenticationService.java
@@ -0,0 +1,37 @@
+package com.izhar.securityalibou.auth;
+
+import com.izhar.securityalibou.config.JwtService;
+import com.izhar.securityalibou.user.Role;
+import com.izhar.securityalibou.user.User;
+import com.izhar.securityalibou.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    }
+}
