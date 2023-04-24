e155dc2ecf87ce3500b0d506cb81578392f413d3
izharj <eezhar@gmail.com>
2023-04-24 17:20:25 +0800

    created applicationconfig and securityconfig classes

--- src/main/java/com/izhar/securityalibou/config/JwtAuthenticationFilter.java
+++ src/main/java/com/izhar/securityalibou/config/JwtAuthenticationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +21,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -26,13 +32,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwt;
         final String userEmail;
 
+        // if no authHeader, the filter will pass the request along the filter chain
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
         }
 
+        // if the header is present then, we extract the jwt token
         jwt = authHeader.substring(7); // extract the token from the header after "Bearer "
         userEmail = jwtService.extractUsername(jwt);// TODO: extract the userEmail from JWT Token
+        // check userEmail is not null & if user has not been authenticated previously
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            // check user from DB
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            // check if token is valid
+            if (jwtService.isTokenValid(jwt,userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                // now to update security context holder
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
 
+        }
+        // finally, pass it along
+        filterChain.doFilter(request, response);
     }
 }
---
+++ src/main/java/com/izhar/securityalibou/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.izhar.securityalibou.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("")// whitelist this list
+                .permitAll()
+                .anyRequest() // any other requests should be authenticated
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // stateless session security
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+}
