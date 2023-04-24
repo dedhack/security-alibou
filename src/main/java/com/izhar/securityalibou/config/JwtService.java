0302aa2cad8250a85081080ea4b66b1311b645bb
izharj <eezhar@gmail.com>
2023-04-24 12:53:09 +0800

    created methods to check on validity of token if it has expired

--- src/main/java/com/izhar/securityalibou/config/JwtService.java
+++ src/main/java/com/izhar/securityalibou/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -44,7 +45,19 @@ public class JwtService {
 
     }
 
-    //
+    // check validity of token
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token))
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
