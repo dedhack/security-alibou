917308e0e2b58c242ce7e3282e2b0e35d66b820f
izharj <eezhar@gmail.com>
2023-04-24 12:44:47 +0800

    Update JwtService.java

--- src/main/java/com/izhar/securityalibou/config/JwtService.java
+++ src/main/java/com/izhar/securityalibou/config/JwtService.java
@@ -1,11 +1,41 @@
 package com.izhar.securityalibou.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    // TODO: Move this applications properties
+    private static final String SECRET_KEY = "66556A586E3272357538782F413F4428472B4B6250645367566B597033733676";
+
     public String extractUsername(String token) {
-        return null;
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
