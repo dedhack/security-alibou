e26976eb04dbc1cb66e4b7f95673c80f64ae0e97
izharj <eezhar@gmail.com>
2023-04-24 12:53:34 +0800

    missing :

--- src/main/java/com/izhar/securityalibou/config/JwtService.java
+++ src/main/java/com/izhar/securityalibou/config/JwtService.java
@@ -48,7 +48,7 @@ public class JwtService {
     // check validity of token
     public boolean isTokenValid(String token, UserDetails userDetails){
         final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token))
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private boolean isTokenExpired(String token) {
